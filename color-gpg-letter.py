8fd41e068a866b8c33d95b11e44ab351af14af87
the_metalgamer <the_metalgamer@hackerspace.lu>
2012-10-27 19:19:23 +0200

    Removed nargs from source and dest

--- color-gpg-letter.py
+++ color-gpg-letter.py
@@ -14,8 +14,8 @@ import decrypt
 parser = argparse.ArgumentParser()
 
 
-parser.add_argument("source", help="The file to encrypt or decrypt (based on extension)", metavar="SOURCE", nargs="?", default=sys.stdin)
-parser.add_argument("dest", help="The destination", metavar="DEST", nargs="?", default=sys.stdout)
+parser.add_argument("source", help="The file to encrypt or decrypt (based on extension)", metavar="SOURCE", default=sys.stdin)
+parser.add_argument("dest", help="The destination", metavar="DEST", default=sys.stdout)
 
 parser.add_argument("-o", "--output", type=str, help="The output file if source is stdin")
 
